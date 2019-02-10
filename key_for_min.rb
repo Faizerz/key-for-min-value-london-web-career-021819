9578c82b7b11f3c15e3878ab9facdd9712f62ac8
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-10 23:49:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,10 @@ def key_for_min_value(name_hash)
   min_value = 1000000
   min_key = 0
   name_hash.each do |key, value|
-    if value < min_value 
+    if key == nil && value == nil
+      return nil
+      
+    elsif value < min_value
       min_value = value
       min_key = key
     end
